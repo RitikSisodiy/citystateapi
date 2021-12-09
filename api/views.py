22c7ae913aa5f3206487ddbac60d89e866ac36e9
RitikSisodiy <ritik.s10120@gmail.com>
2021-12-09 16:35:15 +0530

    some fixes in api

--- api/views.py
+++ api/views.py
@@ -6,83 +6,45 @@ from .models import Country, city, cityLocations, state
 def filtermodel(data,sample,resfieldcsv,modelname):
     if sample is not None:
         country = modelname.objects.values()[0]
-        return JsonResponse(country,safe=False)
+        return [country]
+    for f in modelname._meta.fields:
+        if "ForeignKey" in str(type(f)):
+            if data.get(f.name+"__iexact") is not None: 
+                data[f.name] = data[f.name+"__iexact"]
+                data.pop(f.name+"__iexact")
+    pagesize = data.get('pagesize__iexact') if data.get('pagesize__iexact') is not None else 1000
+    if data.get('pagesize__iexact') is not None: data.pop('pagesize__iexact')
     try:
-        del data['data']
+        del data['data__iexact']
     except:
         pass
     arg =resfieldcsv.split(',') if resfieldcsv is not None else []
     print(data)
     print(arg)
-    return modelname.objects.filter(**data).values(*arg)
+    return modelname.objects.filter(**data).order_by('name')[:pagesize].values(*arg)
 
 
 def stateView(request):
-    country = request.GET.get('countryid')
-    sample = request.GET.get('sample')
-    resval = request.GET.get('data')
-    data = {key:request.GET[key] for key in request.GET}
-    if sample is not None:
-        statedata = state.objects.values()[0]
-        return JsonResponse(statedata,safe=False)
-    if country is not None :
-        try:
-            del data['countryid']
-            del data['data']
-        except:
-            pass
-        data['country'] = country
-        arg =resval.split(',') if resval is not None else []
-        print(data)
-        print(arg)
-        country  = state.objects.filter(**data).values(*arg).order_by('name')
-        return JsonResponse(list(country),safe=False)
-    return HttpResponse('country is required',status=404)
-def cityView(request):
-    State = request.GET.get('stateid')
     sample = request.GET.get('sample')
     resval = request.GET.get('data')
     data = {key+"__iexact":request.GET[key] for key in request.GET}
-    if sample is not None:
-        statedata = city.objects.values()[0]
-        return JsonResponse(statedata,safe=False)
-    if State is not None :
-        try:
-            del data['stateid__iexact']
-            del data['data__iexact']
-        except:
-            pass
-        data['state'] = State
-        arg =resval.split(',') if resval is not None else []
-        print(data)
-        print(arg)
-        country  = city.objects.filter(**data).values(*arg).order_by('name')
-        return JsonResponse(list(country),safe=False)
-    return HttpResponse('stateid is required',status=404)
+    data = filtermodel(data,sample,resval,state)
+    return JsonResponse(list(data),safe=False)
+def cityView(request):
+    sample = request.GET.get('sample')
+    resval = request.GET.get('data')
+    data = {key+"__iexact":request.GET[key] for key in request.GET}
+    data = filtermodel(data,sample,resval,city)
+    return JsonResponse(list(data),safe=False)
 def countryView(request):
     sample = request.GET.get('sample')
     resval = request.GET.get('data')
-    data = {key:request.GET[key] for key in request.GET}
+    data = {key+"__iexact":request.GET[key] for key in request.GET}
     data  = filtermodel(data,sample,resval,Country)
     return JsonResponse(list(data),safe=False)
 def locationsView(request):
-    State = request.GET.get('cityid')
     sample = request.GET.get('sample')
     resval = request.GET.get('data')
-    data = {key:request.GET[key] for key in request.GET}
-    if sample is not None:
-        statedata = cityLocations.objects.values()[0]
-        return JsonResponse(statedata,safe=False)
-    if State is not None :
-        try:
-            del data['cityid']
-            del data['data']
-        except:
-            pass
-        data['city'] = State
-        arg =resval.split(',') if resval is not None else []
-        print(data)
-        print(arg)
-        country  = cityLocations.objects.filter(**data).values(*arg).order_by('name')
-        return JsonResponse(list(country),safe=False)
-    return HttpResponse('country is required',status=404)
+    data = {key+"__iexact":request.GET[key] for key in request.GET}
+    data = filtermodel(data,sample,resval,cityLocations)
+    return JsonResponse(list(data),safe=False)
