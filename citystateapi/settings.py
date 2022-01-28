36ef77cbd82fd0305fef74dc9f96e812f512d457
RitikSisodiy <59529659+RitikSisodiy@users.noreply.github.com>
2022-01-28 15:49:27 +0530

    Update settings.py
    add local core

--- citystateapi/settings.py
+++ citystateapi/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ["*"]
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_WHITELIST = (
    'https://sequpackage.cyberinstant.com/',
+   'http://127.0.0.1:8000/'
 )
 
 # Application definition
